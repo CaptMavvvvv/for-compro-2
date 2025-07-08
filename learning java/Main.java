9053982a6be30ef9d0dae90c5c4808057fbb8981
CaptMavvvvv <deathmoonza2017@gmail.com>
2025-07-09 00:33:08 +0700

    save for learning java

---
+++ learning java/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args) {
+      String name = "Beam";
+      int age = 20;
+      int ID = 45375;
+      double fee = 20.55d;
+      char grade =  'B';
+
+      System.out.println("Student name is " + name);
+      System.out.println(age + " years old");
+      System.out.println("ID is " + ID);
+      System.out.println("Student fee : " + fee);
+      System.out.println("Final grade is " + grade);
+  }
+}
---
+++ learning java/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int countdown = 5;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Heppy New Year!!");
+    }
+}
